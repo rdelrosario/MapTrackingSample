1677bd9e7c6007a43feacda282f6f38f7b294f07
Charlin Agramonte <char0394@gmail.com>
2020-05-11 21:45:50 -0400

    Sample updated

---
+++ TrackingSample/Controls/ExtendedGoogleMaps.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace TrackingSample.Controls
+{
+    public class ExtendedMap : Xamarin.Forms.GoogleMaps.Map
+    {
+        public static readonly BindableProperty CalculateCommandProperty =
+            BindableProperty.Create(nameof(CalculateCommand), typeof(ICommand), typeof(MainPage), null, BindingMode.TwoWay);
+
+        public ICommand CalculateCommand
+        {
+            get { return (ICommand)GetValue(CalculateCommandProperty); }
+            set { SetValue(CalculateCommandProperty, value); }
+        }
+
+        public static readonly BindableProperty UpdateCommandProperty =
+          BindableProperty.Create(nameof(UpdateCommand), typeof(ICommand), typeof(MainPage), null, BindingMode.TwoWay);
+
+        public ICommand UpdateCommand
+        {
+            get { return (ICommand)GetValue(UpdateCommandProperty); }
+            set { SetValue(UpdateCommandProperty, value); }
+        }
+
+        public static readonly BindableProperty GetActualLocationCommandProperty =
+          BindableProperty.Create(nameof(GetActualLocationCommand), typeof(ICommand), typeof(MainPage), null, BindingMode.TwoWay);
+
+        public ICommand GetActualLocationCommand
+        {
+            get { return (ICommand)GetValue(GetActualLocationCommandProperty); }
+            set { SetValue(GetActualLocationCommandProperty, value); }
+        }
+
+        public event EventHandler OnCalculate = delegate { };
+        public event EventHandler OnLocationError = delegate {};
+
+        public ExtendedMap()
+        {
+
+            AddMapStyle();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            if(BindingContext !=null)
+            {
+                CalculateCommand = new Command<List<Position>>(Calculate);
+                UpdateCommand = new Command<Position>(Update);
+                GetActualLocationCommand = new Command(async () => await GetActualLocation());
+            }
+        }
+
+        void AddMapStyle()
+        {
+            var assembly = typeof(ExtendedMap).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream($"TrackingSample.MapStyle.json");
+            string styleFile;
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                styleFile = reader.ReadToEnd();
+            }
+
+            MapStyle = MapStyle.FromJson(styleFile);
+        }
+
+
+        async void Update(Xamarin.Forms.GoogleMaps.Position position)
+        {
+            if (Pins.Count == 1 && Polylines != null && Polylines?.Count > 1)
+                return;
+
+            var cPin = Pins.FirstOrDefault();
+
+            if (cPin != null)
+            {
+                cPin.Position = new Position(position.Latitude, position.Longitude);
+                cPin.Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("ic_taxi.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "ic_taxi.png", WidthRequest = 25, HeightRequest = 25 });
+
+                await MoveCamera(CameraUpdateFactory.NewPosition(new Position(position.Latitude, position.Longitude)));
+                var previousPosition = Polylines?.FirstOrDefault()?.Positions?.FirstOrDefault();
+                Polylines?.FirstOrDefault()?.Positions?.Remove(previousPosition.Value);
+            }
+            else
+            {
+                //END TRIP
+                Polylines?.FirstOrDefault()?.Positions?.Clear();
+            }
+
+
+        }
+
+        void Calculate(List<Xamarin.Forms.GoogleMaps.Position> list)
+        {
+            OnCalculate?.Invoke(this, default(EventArgs));
+            Polylines.Clear();
+            var polyline = new Xamarin.Forms.GoogleMaps.Polyline();
+            foreach (var p in list)
+            {
+                polyline.Positions.Add(p);
+
+            }
+            Polylines.Add(polyline);
+            MoveToRegion(MapSpan.FromCenterAndRadius(new Position(polyline.Positions[0].Latitude, polyline.Positions[0].Longitude), Xamarin.Forms.GoogleMaps.Distance.FromMiles(0.50f)));
+
+            var pin = new Xamarin.Forms.GoogleMaps.Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(polyline.Positions.First().Latitude, polyline.Positions.First().Longitude),
+                Label = "First",
+                Address = "First",
+                Tag = string.Empty,
+                Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("ic_taxi.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "ic_taxi.png", WidthRequest = 25, HeightRequest = 25 })
+
+            };
+            Pins.Add(pin);
+            var pin1 = new Xamarin.Forms.GoogleMaps.Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(polyline.Positions.Last().Latitude, polyline.Positions.Last().Longitude),
+                Label = "Last",
+                Address = "Last",
+                Tag = string.Empty
+            };
+            Pins.Add(pin1);
+        }
+
+        async Task GetActualLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.High);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    MoveToRegion(MapSpan.FromCenterAndRadius(
+                        new Position(location.Latitude, location.Longitude), Distance.FromMiles(0.3)));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                OnLocationError?.Invoke(this, default(EventArgs));
+            }
+        }
+
+
+    }
+}
--- TrackingSample/Views/MainPage.xaml.cs
+++ TrackingSample/Views/MainPage.xaml.cs
@@ -13,106 +13,18 @@ namespace TrackingSample
 {
     public partial class MainPage : ContentPage
     {
-        public static readonly BindableProperty CalculateCommandProperty =
-            BindableProperty.Create(nameof(CalculateCommand), typeof(ICommand), typeof(MainPage), null, BindingMode.TwoWay);
-
-        public ICommand CalculateCommand
-        {
-            get { return (ICommand)GetValue(CalculateCommandProperty); }
-            set { SetValue(CalculateCommandProperty, value); }
-        }
-
-        public static readonly BindableProperty UpdateCommandProperty =
-          BindableProperty.Create(nameof(UpdateCommand), typeof(ICommand), typeof(MainPage), null, BindingMode.TwoWay);
-
-        public ICommand UpdateCommand
-        {
-            get { return (ICommand)GetValue(UpdateCommandProperty); }
-            set { SetValue(UpdateCommandProperty, value); }
-        }
+        
 
         public MainPage()
         {
             InitializeComponent();
-            CalculateCommand = new Command<List<Xamarin.Forms.GoogleMaps.Position>>(Calculate);
-            UpdateCommand = new Command<Xamarin.Forms.GoogleMaps.Position>(Update);
-            GetActualLocationCommand = new Command(async () => await GetActualLocation());
-            AddMapStyle();
+
+           
+           
         }
 
-        void AddMapStyle()
-        {
-            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-            var stream = assembly.GetManifestResourceStream($"TrackingSample.MapStyle.json");
-            string styleFile;
-            using (var reader = new System.IO.StreamReader(stream))
-            {
-                styleFile = reader.ReadToEnd();
-            }
-
-            map.MapStyle = MapStyle.FromJson(styleFile);
-        }
-
-        async void Update(Xamarin.Forms.GoogleMaps.Position position)
-        {
-            if (map.Pins.Count == 1 && map.Polylines!=null&& map.Polylines?.Count>1)
-                return;
-
-            var cPin = map.Pins.FirstOrDefault();
-
-            if (cPin != null)
-            {
-                cPin.Position = new Position(position.Latitude, position.Longitude);
-                cPin.Icon = BitmapDescriptorFactory.FromView(new Image() { Source = "ic_taxi.png", WidthRequest = 25, HeightRequest = 25 });
-
-                await map.MoveCamera(CameraUpdateFactory.NewPosition(new Position(position.Latitude, position.Longitude)));
-                var previousPosition = map?.Polylines?.FirstOrDefault()?.Positions?.FirstOrDefault();
-                map.Polylines?.FirstOrDefault()?.Positions?.Remove(previousPosition.Value);
-            }
-            else
-            {
-                //END TRIP
-                map.Polylines?.FirstOrDefault()?.Positions?.Clear();
-            }
-
-
-        }
-
-        void Calculate(List<Xamarin.Forms.GoogleMaps.Position> list)
-        {
-            searchLayout.IsVisible = false;
-            stopRouteButton.IsVisible = true;
-            map.Polylines.Clear();
-            var polyline = new Xamarin.Forms.GoogleMaps.Polyline();
-            foreach (var p in list)
-            {
-                polyline.Positions.Add(p);
-               
-            }
-            map.Polylines.Add(polyline);
-            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(polyline.Positions[0].Latitude, polyline.Positions[0].Longitude), Xamarin.Forms.GoogleMaps.Distance.FromMiles(0.50f)));
-
-            var pin = new Xamarin.Forms.GoogleMaps.Pin
-            {
-                Type = PinType.Place,
-                Position = new Position(polyline.Positions.First().Latitude, polyline.Positions.First().Longitude),
-                Label = "First",
-                Address = "First",
-                Tag = string.Empty,
-                Icon = BitmapDescriptorFactory.FromView(new Image() { Source = "ic_taxi.png", WidthRequest = 25, HeightRequest = 25 })
-
-            };
-            map.Pins.Add(pin);
-            var pin1 = new Xamarin.Forms.GoogleMaps.Pin
-            {
-                Type = PinType.Place,
-                Position = new Position(polyline.Positions.Last().Latitude, polyline.Positions.Last().Longitude),
-                Label = "Last",
-                Address = "Last",
-                Tag = string.Empty
-            };
-            map.Pins.Add(pin1);
-        }
+       
+       
 
         public async void OnEnterAddressTapped(object sender, EventArgs e)
         {
@@ -132,37 +44,21 @@ namespace TrackingSample
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            GetActualLocationCommand.Execute(null);
+            map.GetActualLocationCommand.Execute(null);
         }
 
-        public static readonly BindableProperty GetActualLocationCommandProperty =
-            BindableProperty.Create(nameof(GetActualLocationCommand), typeof(ICommand), typeof(MainPage), null, BindingMode.TwoWay);
-
-        public ICommand GetActualLocationCommand
+        void OnCalculate(System.Object sender, System.EventArgs e)
         {
-            get { return (ICommand)GetValue(GetActualLocationCommandProperty); }
-            set { SetValue(GetActualLocationCommandProperty, value); }
+            searchLayout.IsVisible = false;
+            stopRouteButton.IsVisible = true;
         }
 
-        async Task GetActualLocation()
+        async void OnLocationError(System.Object sender, System.EventArgs e)
         {
-            try
-            {
-                var request = new GeolocationRequest(GeolocationAccuracy.High);
-                var location = await Geolocation.GetLocationAsync(request);
-
-                if (location != null)
-                {
-                    map.MoveToRegion(MapSpan.FromCenterAndRadius(
-                        new Position(location.Latitude, location.Longitude), Distance.FromMiles(0.3)));
-
-                }
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error", "Unable to get actual location", "Ok");
-            }
+            await DisplayAlert("Error", "Unable to get actual location", "Ok");
         }
+
+
 
     }
 }
